c41bb3d54f6599c6b04546fbfe577696ba42103e
WitBlue6 <107780822+WitBlue6@users.noreply.github.com>
2023-06-07 01:11:42 +0800

    Add files via upload

--- Core/sb.py
+++ Core/sb.py
@@ -77,6 +77,13 @@ class LoginPage:
 class MainPage:
     """
     登陆后的主面板
+    目前包含:
+        self.name 姓名
+        self.age 年龄
+        self.university 大学
+        self.wechat 微信
+        self.QQ QQ号码
+        self.phone 电话号码
     """
     def __init__(self, master: tk.Tk):
         self.root = master
@@ -98,7 +105,7 @@ class MainPage:
 
     def create_page(self):
         """
-        页面
+        创建主页面以及菜单栏
         :return:
         """
         self.about_frame = tk.Frame(self.root)
@@ -138,57 +145,53 @@ class MainPage:
         tk.Entry(self.write_frame, textvariable=self.phone).grid(row=12, column=2)
         tk.Button(self.write_frame, text='录入', command=self.write_in).grid(row=13)
 
+        self.all_frame = tk.Frame()
+        self.pagenum = 0  # 查看栏的当前浏览页
+        self.recent_pagenum = 0  # 上一次浏览的页面
+        tk.Label(self.all_frame, text='所有录入人员如下').grid(row=0)
 
         # 菜单栏
         menubar = tk.Menu(self.root)
+        menubar.add_command(label='查看', command=self.show_all)
         menubar.add_command(label='查询', command=self.show_admit)
         menubar.add_command(label='录入', command=self.show_write)
         menubar.add_command(label='关于', command=self.show_about)
+
         self.root['menu'] = menubar
 
     def search(self):
+        """
+        通过姓名搜索录入的信息
+        并将搜索结果展示
+        :return:
+        """
         name = self.name.get()
         with open('Person.txt', 'r') as f:
             flag = False
             for data in f.readlines():
                 if name == data.split(' ')[0]:
-                    self.s_name = name
-                    try:
-                        self.s_age = data.split(' ')[1]
-                    except:
-                        print('No age')
-                        messagebox.showinfo(title='提示', message='部分数据缺失!')
-                    try:
-                        self.s_uni = data.split(' ')[2]
-                    except:
-                        print('No university')
-                        messagebox.showinfo(title='提示', message='部分数据缺失!')
-                    try:
-                        self.s_wechat = data.split(' ')[3]
-                    except:
-                        print('No wechat')
-                        messagebox.showinfo(title='提示', message='部分数据缺失!')
-                    try:
-                        self.s_QQ = data.split(' ')[4]
-                    except:
-                        print('No QQ')
-                        messagebox.showinfo(title='提示', message='部分数据缺失!')
-                    try:
-                        self.s_phone = data.split(' ')[5]
-                    except:
-                        print('No phone number')
-                        messagebox.showinfo(title='提示', message='部分数据缺失!')
+                    self.s_name = data.split(' ')[0]
+                    self.s_age = data.split(' ')[1]
+                    self.s_uni = data.split(' ')[2]
+                    self.s_wechat = data.split(' ')[3]
+                    self.s_QQ = data.split(' ')[4]
+                    self.s_phone = data.split(' ')[5]
 
                     flag = True
                     break
             # 找到
             if flag:
                 messagebox.showinfo(title='提示', message='查询成功')
-                self.show_search()
+                self.show_search(False)
             else:
                 messagebox.showinfo(title='提示', message='未查询到该对象')
 
     def write_in(self):
+        """
+        录入信息
+        至少录入姓名，否则录入失败
+        :return:
+        """
         name = self.name.get()
         age = self.age.get()
         uni = self.university.get()
@@ -197,13 +200,17 @@ class MainPage:
         phone = self.phone.get()
 
         with open('Person.txt', 'a') as f:
-            f.writelines([name,' ', age, ' ', uni, ' ', wec, ' ', qq, ' ', phone, '\n'])
-        messagebox.showinfo(title='提示', message='录入成功')
+            if name:
+                f.writelines([name,' ', age, ' ', uni, ' ', wec, ' ', qq, ' ', phone, '\n'])
+                messagebox.showinfo(title='提示', message='录入成功')
+            else:
+                messagebox.showinfo(title='提示', message='录入失败！至少输入一个姓名!')
         self.write_frame.pack_forget()
 
     def show_about(self):
         """
         展示关于栏
+        一些关于该程序的基本信息
         :return:
         """
         self.about_frame.pack()
@@ -217,6 +224,7 @@ class MainPage:
     def show_admit(self):
         """
         展示查询栏
+        查询录入的信息
         :return:
         """
         self.admit_frame.pack()
@@ -228,6 +236,11 @@ class MainPage:
             pass
 
     def show_write(self):
+        """
+        展示录入栏
+        录入新的人员信息
+        :return:
+        """
         self.write_frame.pack()
         self.admit_frame.pack_forget()
         self.about_frame.pack_forget()
@@ -236,7 +249,66 @@ class MainPage:
         except:
             pass
 
-    def show_search(self):
+    def show_all(self):
+        """
+        展示当前已录入的所有人员信息名单
+        通过换页按钮进行换页
+        :return:
+        """
+        try:
+            self.search_frame.destroy()
+        except:
+            pass
+        with open('Person.txt', 'r') as f:
+            self.All_People = []
+            each = dict()
+            for Person in f.readlines():
+                if Person.split(' ')[0]:
+                    each = each.copy()
+                    each["name"] = Person.split(' ')[0]
+                    each["age"] = Person.split(' ')[1]
+                    each["uni"] = Person.split(' ')[2]
+                    each["wechat"] = Person.split(' ')[3]
+                    each["QQ"] = Person.split(' ')[4]
+                    each["phone"] = Person.split(' ')[5]
+
+                    self.All_People.append(each)
+
+        self.s_name = self.All_People[self.pagenum]["name"]
+        self.s_uni = self.All_People[self.pagenum]["uni"]
+        self.s_age = self.All_People[self.pagenum]["age"]
+        self.s_wechat = self.All_People[self.pagenum]["wechat"]
+        self.s_QQ = self.All_People[self.pagenum]["QQ"]
+        self.s_phone = self.All_People[self.pagenum]["phone"]
+
+        self.show_search(True)
+
+
+    def lastpage(self):
+        """
+        查看栏回到上一页
+        :return:
+        """
+        self.recent_pagenum = self.pagenum
+        if self.pagenum > 0:
+            self.pagenum -= 1
+            self.show_all()
+    def nextpage(self):
+        """
+        查看栏进入下一页
+        :return:
+        """
+        self.recent_pagenum = self.pagenum
+        if self.pagenum < len(self.All_People)-1:
+            self.pagenum += 1
+            self.show_all()
+    def show_search(self, Button: bool):
+        """
+        展示搜索的结果
+        若缺失信息，缺失的那一栏为空
+        :param Button:
+        :return:
+        """
         self.search_frame = tk.Frame()
 
         tk.Label(self.search_frame).grid(row=0)
@@ -257,12 +329,20 @@ class MainPage:
         tk.Label(self.search_frame).grid(row=10)
         tk.Label(self.search_frame, text='手机:').grid(row=11, column=1)
         tk.Label(self.search_frame, text=self.s_phone).grid(row=11, column=2)
+        if Button:
+            tk.Button(self.search_frame, text='上一页',
+                      command=self.lastpage, height=2).grid(row=12, column=1, padx=(0,30))
+            tk.Button(self.search_frame, text='下一页',
+                      command=self.nextpage, height=2).grid(row=12, column=2, padx=(30,0))
+            tk.Label(self.search_frame, text="第{}页".format(self.pagenum+1), height=2).grid(row=13, column=2, padx=(0, 90))
 
         self.search_frame.pack()
         self.admit_frame.pack_forget()
+        self.about_frame.pack_forget()
+        self.write_frame.pack_forget()
 
 root = tk.Tk()
-LoginPage(root)
+MainPage(root)
 
 root.mainloop()
 
